cdeda1b2e2e7a3b538b50067ac61fd610db75209
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-08-20 20:46:32 -0600

    basic template, views, and general schema tested

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ Coming soon<br>
 - Django
 - Python
 - PostgreSQL
-- Heroku<br>
+- Heroku
+- Normalize
+<br>
 
 ### ERD
 Coming soon<br>
@@ -18,6 +20,14 @@ Coming soon<br>
 ### Resources
 API: fatsecretPlatform<br>
 
+### Color Pallette
+![Colors](https://i.pinimg.com/564x/fb/5c/ee/fb5ceec9fc8546d300c65c1267e6c017.jpg)
+
 ### Dev Notes
 Database name: dream-engineer<br>
+Drk Pink: #E46F80
+Lgt Pink: #E9CAD2
+White: #FFF
+Grey: #D2D2D0
+Black: #000
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,28 @@
+from unicodedata import name
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('DREAM ENGINEER')
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse('About page')
+    return render(request, 'about.html')
+
+class Goal:
+    def __init__(self, name, description, motivation, measure, start_date, target_date, roadblock):
+        self.name = name
+        self.description = description
+        self.motivation = motivation
+        self.measure = measure
+        self.start_date = start_date
+        self.target_date = target_date
+        self.roadblock = roadblock
+
+goals = [
+    Goal('Loss Weight', 'Lose ten pounds', 'Look good next summer in bathing suit', 173, '8/20/22', '5/1/23', 'none' ),
+    Goal('Eat healthy', 'Reduce sugar to help weight loss goal', 'Look good in a bathing suit next summer', 1200, '8/20/22', '5/1/23', 'none'),
+    Goal('Code Better', 'Increase time coding to skill up', 'To get a higher paying job', 25, '8/20/22', '8/20/23', 'none')
+]        
+
+def goals_index(request):
+    return render(request, 'goals/index.html', {'goals': goals})
