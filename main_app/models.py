66370aaab823f04bfc2616d0ba73a0465f253349
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-08-22 20:36:29 -0600

    created goal model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
+
 # Create your models here.
+class Goal(models.Model):
+    name: models.CharField(max_length=100)
+    reason: models.TextField()
+    start_date: models.DateTimeField(auto_now_add=True)
+    end_date: models.DateTimeField()
+    start_weight: models.IntegerField()
+    end_weight: models.IntegerField()
+    exercise: ArrayField(models.CharField(max_length=100, blank=True))
+    completed: models.BooleanField()
+    
